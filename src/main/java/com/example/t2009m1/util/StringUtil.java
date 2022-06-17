ee61eaca90803fd2dac4590cfe3d774675f99740
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2022-06-17 07:07:27 +0700

    Initial commit

---
+++ src/main/java/com/example/t2009m1/util/StringUtil.java
@@ -0,0 +1,7 @@
+package com.example.t2009m1.util;
+
+import java.util.regex.Pattern;
+
+public class StringUtil {
+
+}
---
+++ src/test/java/com/example/t2009m1/T2009M1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t2009m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2009M1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
